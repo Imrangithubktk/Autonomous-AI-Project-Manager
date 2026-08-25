bd76c7662572d3f1725fca9133a2da0690970a4b
Imran ullah <146697699+Imrangithubktk@users.noreply.github.com>
2026-08-25 08:05:43 +0500

    Update model in get_llm function

--- app/ai/groq_client.py
+++ app/ai/groq_client.py
@@ -1,15 +1,10 @@
 import os
-
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-
 load_dotenv()
-
-
 def get_llm():
-
     return ChatGroq(
-        model="llama-3.3-70b-versatile",
+        model="openai/gpt-oss-120b",
         temperature=0.3,
         api_key=os.getenv("GROQ_API_KEY")
     )
