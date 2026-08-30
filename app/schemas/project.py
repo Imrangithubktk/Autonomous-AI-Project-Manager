09840bd286d5606864edb56e733713d5b7d9682f
Imran ullah <146697699+Imrangithubktk@users.noreply.github.com>
2026-08-30 22:01:50 +0500

    Update project.py

--- app/schemas/project.py
+++ app/schemas/project.py
@@ -3,12 +3,14 @@ from datetime import date
 
 class ProjectCreate(BaseModel):
     project_name: str
+    owner_name: str
     description: str
     start_date: date
     end_date: date
     status: str = "Active"
 class ProjectUpdate(BaseModel):
     project_name: str
+    owner_name: str
     description: str
     start_date: date
     end_date: date
